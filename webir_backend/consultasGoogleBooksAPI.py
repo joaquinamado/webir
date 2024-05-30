8ff985e38ad2823599e96608916aec4998075d73
joaquinamado <joaquin.amado26@gmail.com>
2024-05-30 16:46:28 -0300

    fix

--- webir_backend/consultasGoogleBooksAPI.py
+++ webir_backend/consultasGoogleBooksAPI.py
@@ -1,10 +1,14 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
-API_KEY = os.environ.get("API_KEY")
+load_dotenv()
+
+
+API_KEY = str(os.getenv("API_KEY"))
 URL_GOOGLE_BOOKS = "https://www.googleapis.com/books/v1/"
 
-response = requests.get(URL_GOOGLE_BOOKS +"volumes?q=flowers+inauthor:keyes&key="+API_KEY) 
+response = requests.get(URL_GOOGLE_BOOKS +"volumes?q=flowers+inauthor:keyes&key="+API_KEY)
 json_response = response.json()
 print(json.dumps(json_response, indent=4))
